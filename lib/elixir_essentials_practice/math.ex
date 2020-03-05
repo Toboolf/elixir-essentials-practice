077767d5a61ececb425eada741ec89aa44e9a4e3
Jose Cruz <floobot@hotmail.com>
2020-03-05 11:20:19 -0600

    coding grat common divisor in math module

--- lib/elixir_essentials_practice/math.ex
+++ lib/elixir_essentials_practice/math.ex
@@ -5,4 +5,8 @@ defmodule ElixirEssentialsPractice.Math do
     def kind(0), do: :zero
     def kind(n) when n < 0, do: :negative
 
+    def gcd(n,n), do: n
+    def gcd(n,m) when n > m, do: gcd(n - m, m) 
+    def gcd(n,m) when m > n, do: gcd(n, m - n)
+
 end

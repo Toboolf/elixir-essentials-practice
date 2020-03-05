1d731f3c617c630a8f0b93d8572da4fa895dbc36
Jose Cruz <floobot@hotmail.com>
2020-03-05 12:44:22 -0600

    coding master solving part 2 of excercise 1

--- lib/elixir_essentials_practice/excercise_1.ex
+++ lib/elixir_essentials_practice/excercise_1.ex
@@ -1,9 +1,17 @@
 defmodule SantasFloorMapping do
 
-    def read_santas_map(string), do: count_next(String.split(string, ""), 0)
+    def read_santas_map(string) do
+        string
+        |> String.split("")
+        |> count_next(0, -1)
+    end
 
-    def count_next([], count), do: count
-    def count_next([h | t], count), do: count_next(t, count + upstairs_downstairs(h))
+    def count_next([], count, _), do: count
+    def count_next([h | t], count, index) when count == -1 do
+        IO.inspect "Santa's in the basement at instruction #{index}"
+        count_next(t, count + upstairs_downstairs(h), index + 1)
+    end
+    def count_next([h | t], count, index), do: count_next(t, count + upstairs_downstairs(h), index + 1)
 
     def upstairs_downstairs("("), do: 1
     def upstairs_downstairs(")"), do: -1
